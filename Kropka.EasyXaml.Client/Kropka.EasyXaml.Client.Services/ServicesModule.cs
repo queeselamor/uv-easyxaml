80ec52cc99939d87b889bab739bead5d94894753
Uladzislau Vyskvarka <uladzislau.vyskvarka@gmail.com>
2023-06-01 17:33:21 +0300

    Fix ServicesModule

--- Kropka.EasyXaml.Client/Kropka.EasyXaml.Client.Services/ServicesModule.cs
+++ Kropka.EasyXaml.Client/Kropka.EasyXaml.Client.Services/ServicesModule.cs
@@ -1,18 +1,15 @@
 ﻿using Prism.Ioc;
 using Prism.Modularity;
 
-namespace Kropka.EasyXaml.Client.Services
+namespace Kropka.EasyXaml.Client.Services;
+
+public class ServicesModule : IModule
 {
-    public class ServicesModule : IModule
+    public void OnInitialized(IContainerProvider containerProvider)
     {
-        public void OnInitialized(IContainerProvider containerProvider)
-        {
+    }
 
-        }
-
-        public void RegisterTypes(IContainerRegistry containerRegistry)
-        {
-
-        }
+    public void RegisterTypes(IContainerRegistry containerRegistry)
+    {
     }
 }
