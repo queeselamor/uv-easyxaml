c87842f4e02915e544574d18bbc085b759fcc5ab
Uladzislau Vyskvarka <uladzislau.vyskvarka@gmail.com>
2023-06-08 18:32:55 +0300

    Added new styles

--- Kropka.EasyXaml.Client/Kropka.EasyXaml.Client.Views/Views/WelcomeScreenView.xaml.cs
+++ Kropka.EasyXaml.Client/Kropka.EasyXaml.Client.Views/Views/WelcomeScreenView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Kropka.EasyXaml.Client.Infrastructure.Interfaces.Views;
+﻿using System.Windows;
+using System.Windows.Input;
+using Kropka.EasyXaml.Client.Infrastructure.Interfaces.Views;
 
 namespace Kropka.EasyXaml.Client.Views.Views;
 
