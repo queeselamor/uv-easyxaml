e4a7f5d8418c7ed7bfa85bc3fb1ae8235905f727
Uladzislau Vyskvarka <uladzislau.vyskvarka@gmail.com>
2023-06-15 23:35:33 +0300

    Add warning message

--- Kropka.EasyXaml.Client/Kropka.EasyXaml.Client.Infrastructure/Constants/ContentConstants.cs
+++ Kropka.EasyXaml.Client/Kropka.EasyXaml.Client.Infrastructure/Constants/ContentConstants.cs
@@ -11,5 +11,6 @@ public class ContentConstants
     public const string NoButtonTitle = "No";
     public const string LoadingDefaultTitle = "Loading...";
     public const string ConvertingTitle = "Converting...";
+    public const string CanvasConvertWarning = "The file couldn't be converted to Canvas format. DrawingGroup accessible only.";
     #endregion
 }
